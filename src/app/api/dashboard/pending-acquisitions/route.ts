c739e09b84fff82a6782721e888c81a549f29c4b
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-04-11 10:53:23 -0300

    compras pendentes

---
+++ src/app/api/dashboard/pending-acquisitions/route.ts
@@ -0,0 +1,52 @@
+import prisma from "@db/db"
+import { NextResponse, NextRequest } from "next/server"
+
+
+//helpers
+const getUserByToken = require('../../../helpers/get-user-by-token')
+const getToken = require('../../../helpers/get-token')
+
+export async function GET(req: NextRequest) {
+    const token = getToken(req)
+    const tokenUser = await getUserByToken(token)
+
+    if(!tokenUser.admin) {
+        return NextResponse.json(
+            {
+                message: "Acesso negado!"
+            },
+            {
+                status: 422
+            }
+        )
+    }
+
+    try {
+        const acquisitions = await prisma.acquisition.findMany({
+            where: {
+                finished: false
+            } 
+        })
+
+        if(!acquisitions) {
+            return NextResponse.json(
+                {
+                    message: "não existe vendas pendentes"
+                },
+                {
+                    status: 404
+                }
+            )
+        }
+        return NextResponse.json({message: "OK", acquisitions})
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
