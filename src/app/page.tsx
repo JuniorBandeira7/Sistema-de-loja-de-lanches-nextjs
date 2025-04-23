ded7c6ea520776203b06a22c750778b02ea2563c
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-04-23 18:50:30 -0300

    testando header e section do header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Card from "./components/Card"
+import Header from "./components/Header"
 
 const description = "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
 const number = 23.50
@@ -6,6 +7,13 @@ const number = 23.50
 export default function Home() {
   return (
     <>
+      <Header></Header>
+      <section className="h-[600px] relative flex items-center">
+        <img src="/s1.jfif" alt="" className="object-cover w-full h-full brightness-50 absolute"/>
+        <div className="w-full relative flex items-center content-center">
+          <h1 className="text- text-white">Aqui vai ficar vewsvew</h1>
+        </div>
+      </section>
       <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
     </>
   )
