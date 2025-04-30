62e3a6e94b5db88efcb8dad1a634f613fe5caf1b
JuniorBandeira7 <juliobandeira77@gmail.com>
2025-04-29 22:01:53 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Card from "./components/Card"
 import Header from "./components/Header"
+import PhotoSection from "./components/PhotoSection"
 
 const description = "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."
 const number = 23.50
@@ -8,13 +9,21 @@ export default function Home() {
   return (
     <>
       <Header></Header>
-      <section className="h-[600px] relative flex items-center">
-        <img src="/s1.jfif" alt="" className="object-cover w-full h-full brightness-50 absolute"/>
-        <div className="w-full relative flex items-center content-center">
-          <h1 className="text- text-white">Aqui vai ficar vewsvew</h1>
+      <PhotoSection></PhotoSection>
+      <main className="w-full flex justify-center">
+        <div className="lg:w-[70%] sm:w-[90%] flex items-center flex-col mt-16 bg-gray-100 rounded-lg pr-4 pl-4">
+          <h1 className="text-5xl font-bold mt-2">Burguers</h1>
+          <div className="grid grid-cols-1 gap-2 sm:gap-4 sm:grid-cols-2 mt-8">
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+            <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+          </div>
         </div>
-      </section>
-      <Card photoUrl="/ham.jpg" productName="Hamburguer" price={number.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} description={description}></Card>
+      </main>
     </>
   )
 }
